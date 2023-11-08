ccf6add1ab3ffe591622c5958225133a767cc611
Virginia <viorbe20@gmail.com>
2023-11-08 14:18:44 +0100

    Refact Repetitivas ex8

--- ej-repetitivas/repetitivas_8.py
+++ ej-repetitivas/repetitivas_8.py
@@ -6,12 +6,24 @@ Author: Virginia Ordoño Bernier
 """
 print("\nA continuación, el programa te indicará si una cadena contiene una subcadena.")
 
-string = input("\nIntroduce  la cadena: ")
+string = input("\nIntroduce la cadena: ")
 substring = input("Introduce la subcadena: ")
 
 
-if substring in string:
-    print("\nLa cadena contiene la subcadena.")
+for i in range(len(string) - len(substring) + 1): # (len(string) - len(substring) + 1) == last index where it is possible to find substring
+    
+    match = True
+    print('i->' , i)
+    for j in range(len(substring)): # iterates through the substring 
+        
+        if string[i + j] != substring[j]:  # compares the characters of the substring with the characters of the string
+            print('ij->' , i+j)
+            match = False
+            break
+    
+    if match == True:
+        print("La cadena contiene la subcadena.")
+        break
     
 else:
-    print("\nLa cadena no contiene la subcadena.")
+    print("La cadena no contiene la subcadena.")
