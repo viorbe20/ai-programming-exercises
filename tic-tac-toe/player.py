8db11875826b9917e3455cce3656c7999feac3b3
Virginia <viorbe20@gmail.com>
2023-12-25 12:30:50 +0100

    add get_move methods

--- tic-tac-toe/player.py
+++ tic-tac-toe/player.py
@@ -14,11 +14,26 @@ class RandomComputerPlayer(Player):
         super().__init__(letter)
         
     def get_move(self, game):
-        pass
+        # choose an empty random spot on the board
+        square = random.choice(game.available_moves())
+        return square
 
 class HumanPlayer(Player):
     def __init__(self, letter):
         super().__init__(letter)
         
     def get_move(self, game):
-        pass
+        # human can choose an empty spot on the board
+        valid_square = False
+        val  = None
+        
+        # while not valid square, choose an empty spot on the board
+        while not valid_square:
+            square = input(f'Turno de {self.letter}. Introduce una posición de 0 a 9: ')
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True
+            except ValueError:
+                print('Posición incorrecta. Inténtalo de nuevo.')
