6423c6923aaf7ee1093e5dfbe0a37a288667d516
Virginia <viorbe20@gmail.com>
2023-12-25 11:48:38 +0100

    add class player tic tac toe

---
+++ tic-tac-toe/player.py
@@ -0,0 +1,24 @@
+import math
+import random
+
+class Player:
+    def __init__(self, letter):
+        # letter can be both x and o
+        self.letter = letter
+        
+    def get_move(self, game):
+        pass
+    
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+        
+    def get_move(self, game):
+        pass
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+        
+    def get_move(self, game):
+        pass
---
+++ tic-tac-toe/readme.md
@@ -0,0 +1,11 @@
+# Tic-Tac-Toe
+
+## Contenidos
+
+### 1. Resumen 
+
+El juego está disponible para diferentes tipos de jugadores:
+- Humamo vs. maquina
+- Humano vs. humano
+- Máquina vs. máquina 
+
