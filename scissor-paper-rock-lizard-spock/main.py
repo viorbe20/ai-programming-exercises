f7b36bbfddc459d99eb216c071244c3bac730e36
Virginia <viorbe20@gmail.com>
2024-01-03 18:22:42 +0100

    add scissor paper game

---
+++ scissor-paper-rock-lizard-spock/main.py
@@ -0,0 +1,9 @@
+'''
+Partiendo del código disponible en fichero adjunto,
+añade la funcionalidad necesaria para ofrecer la variante lagarto, Spock del juego piedra, papel o tijeras.
+Usa las diferentes situaciones de juego, junto con su resultado, del archivo victories.xml que se adjunta.
+Sustituye el diccionario Victories por alguna referencia que permita acceder al contenido de victories.xml.
+
+@author - Virginia Ordoño Bernier
+@date - January 2024
+'''
