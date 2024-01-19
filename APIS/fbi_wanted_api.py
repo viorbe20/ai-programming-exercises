19afd0dbeb540f5f8ea5e62a222a8735ecec4db6
Virginia <viorbe20@gmail.com>
2024-01-19 12:30:34 +0100

    ex wanted fbi. Waiting repeately

--- APIS/fbi_wanted_api.py
+++ APIS/fbi_wanted_api.py
@@ -1,58 +1,62 @@
-'''
-El FBI tiene recorte de personal informático y solicitan nuestra ayuda, 
-quieren saber cuantos fugitivos tienes registrados en cada una de sus oficinas, 
-para ello han habilitado una API a la que puedes acceder desde aquí (https://www.fbi.gov/wanted/api).
-
-El programa debe mostrar el nombre de cada oficina (ordenado) y la cantidad de fugitivos registrados. 
-También debe mostrar la cantidad de fugitivos no registrados en ninguna oficina. 
-
-Ten en cuenta que cada consulta muestra un número limitado de registros, 
-vas a tener que hacer consultas iterativas enviando como parámetro la página de la consulta hasta que ya no queden páginas que consultar.
-'''
-
 import requests
+import time
 
 def main():
-    wanted_result = get_wanted_by_office()
-    print(wanted_result)
+    all_results = get_all_results()
+    get_wanted_by_office(all_results)
 
-def get_wanted_by_office():
-    
-    response = requests.get('https://api.fbi.gov/wanted/v1/list')
-    
-    if response.status_code == 200:
-        data = response.json()
-        items = data.get('items', [])
+def get_all_results():
+    url_base = "https://api.fbi.gov/wanted/v1/list"
+    page = 1
+    all_results = []
 
-        field_offices_dic = {}
+    while True:
+        url = f"{url_base}?page={page}"
 
-        for item in items:
+        response = requests.get(url)
 
-            field_offices = item.get('field_offices')
+        if response.status_code == 200:
+            data = response.json()
 
-            # Check if 'field_offices' is not None before iterating
-            if field_offices is not None:
-                for field_office in field_offices:
-                    if field_office in field_offices_dic:
-                        field_offices_dic[field_office] += 1
-                    else:
-                        field_offices_dic[field_office] = 1
+            # Check if there are results
+            if data.get('items'):
+                all_results.extend(data['items'])
+                page += 1
             else:
-                if 'none' in field_offices_dic:
-                    field_offices_dic['Ninguna'] += 1
+                break
+        elif response.status_code == 429:
+            # Rate limit exceeded, wait for a while and try again
+            print(f"Rate limit exceeded. Waiting for 10 seconds...")
+            time.sleep(10)
+        else:
+            print(f"Error en la solicitud: {response.status_code}")
+            break
+    return all_results
+
+def get_wanted_by_office(results):
+    field_offices_dic = {}
+
+    for item in results:
+        field_offices = item.get('field_offices')
+
+        # Check if 'field_offices' is not None before iterating
+        if field_offices is not None:
+            for field_office in field_offices:
+                if field_office in field_offices_dic:
+                    field_offices_dic[field_office] += 1
                 else:
-                    field_offices_dic['Ninguna'] = 1
+                    field_offices_dic[field_office] = 1
+        else:
+            if 'none' in field_offices_dic:
+                field_offices_dic['none'] += 1
+            else:
+                field_offices_dic['none'] = 1
 
-        print("\nListado de fugitivos por oficina:")
-        print('-'*30)
+    print("\nListado de fugitivos por oficina:")
+    print('-'*30)
 
-        for field_office, count in sorted(field_offices_dic.items()):
-            print(f"{field_office.upper()}: {count}")
-    else:
-        print(f"Error al hacer la petición: {response.status_code}")
+    for field_office, count in sorted(field_offices_dic.items()):
+        print(f"{field_office.upper()}: {count}")
 
-
-
-    
 if __name__ == '__main__':
     main()
