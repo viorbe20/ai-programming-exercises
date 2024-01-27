7afb36a5c04a4048f89dcff5100bba994f99293b
Virginia <viorbe20@gmail.com>
2024-01-27 13:35:02 +0100

    wines gui v1

--- APIS/wines_api/main copy.py
+++ APIS/wines_api/main copy.py
@@ -1,3 +1,12 @@
+'''
+Uso del framework Flask para crear una API web,
+la cual predice la calidad de un vino 
+utilizando un modelo de clasificación de machine learning 
+cargado desde un archivo pickle.
+
+author: Virginia Ordoño Bernier
+date: January 2024
+'''
 from flask import Flask, render_template, request, jsonify, make_response
 import pickle
 import os
@@ -9,34 +18,14 @@ script_dir = os.path.dirname(__file__)
 model_path = os.path.join(script_dir, 'model', 'wine_model.pkl')
 model = pickle.load(open(model_path, 'rb'))
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route('/', methods=['GET'])
 def home():
-    if request.method == 'POST':
-        # get parameters from form
-        alcohol = request.form['alcohol']
-        sulphates = request.form['sulphates']
-
-        # validate parameters
-        if alcohol and sulphates:
-            try:
-                alcohol = float(alcohol)
-                sulphates = float(sulphates)
-
-                # model prediction
-                quality = int(model.predict([[alcohol, sulphates]])[0])
-                return render_template('resultado.html', alcohol=alcohol, sulphates=sulphates, quality=quality)
-
-            except ValueError:
-                # Exception if parameters != digits
-                return render_template('home.html', message='Los parámetros deben ser números')
-        else:
-            # Not enough parameters
-            return render_template('home.html', message='Se requieren los parámetros alcohol y sulphates')
-
     return render_template('home.html')
 
 @app.route('/winequality', methods=['GET'])
 def wine_quality():
+    
     # get parameters
     alcohol = request.args.get('alcohol')
     sulphates = request.args.get('sulphates')
@@ -50,7 +39,7 @@ def wine_quality():
             # model prediction
             quality = int(model.predict([[alcohol, sulphates]])[0])
             return jsonify({'quality': quality}), 200
-
+        
         except ValueError:
             # Exception if parameters != digits
             return jsonify({'message': 'Los parámetros deben ser números'}), 400
@@ -59,7 +48,7 @@ def wine_quality():
         return jsonify({'message': 'Se requieren los parámetros alcohol y sulphates'}), 400
 
 @app.errorhandler(404)
-def not_found(error):
+def not_found():
     # Non existing route
     return make_response(jsonify({'message': 'Recurso no encontrado'}), 404)
 
